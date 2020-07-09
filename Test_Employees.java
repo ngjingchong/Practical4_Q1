f74767d031f53c9b5b6b950c9b98220a434ca57c
ngjingchong <66916397+ngjingchong@users.noreply.github.com>
2020-07-09 10:19:53 +0800

    Update Test_Employees.java

--- Test_Employees.java
+++ Test_Employees.java
@@ -6,7 +6,6 @@
  * @version 1.00 2020/7/9
  */
 
-
 public class Test_Employees {
 
     public static void main (String [] args) {
@@ -14,20 +13,18 @@ public class Test_Employees {
     	Employee emp2 = new Employee("Xiao hong");
     	emp2.setSalary(4600.00);
     	
-    	System.out.println("Salary (old): " + emp1.getSalary());
+    	System.out.println("Before raise Salary (" + emp1.getName() + "): " + emp1.getSalary());
     	emp1.raiseSalary(8.0);
-   		System.out.println("Salary (new): " + emp1.getSalary());
+   		System.out.println("Raised Salary (" + emp1.getName() + "): " + emp1.getSalary());
     	
+    	System.out.print("Employees with highest salary: ");
     	if(emp1.getSalary()>emp2.getSalary()){
-            System.out.println("name: " + emp1.getName()+ " salary: " + emp1.getSalary());
+            System.out.println(emp1.getName());
         }
         else{
-            System.out.println("name: " + emp2.getName()+ "salary: " + emp2.getSalary());
+            System.out.println(emp2.getName());
         }
         
         System.out.println("Total Salary: " + (emp1.getSalary()+emp2.getSalary()));
-        
     }
-    
-    
 }
