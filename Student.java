436adfd35acce465dd3e42d42f4e44192513bd05
ngjingchong <66916397+ngjingchong@users.noreply.github.com>
2020-07-16 09:58:15 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,25 +6,25 @@
  * @version 1.00 2020/7/12
  */
 
-
 public class Student {
 	private String id;
 	private String name;
 	private int quizCount;
-	private static double totalScore;
+	private int totalScore;
 		
     public Student() {
-    String id=" ";
-	String name=" ";
-	int quizCount=0;
-	double totalScore=0.0;
+    	//leave it empty since question has no specify any defualt value
+    //String id=" ";
+	//String name=" ";
+	//quizCount=0;
+	//totalScore=0;
     }
     
     public Student(String studID, String new_name) {
     id=studID;
 	name=new_name;
 	quizCount=0;
-	totalScore=0.0;
+	totalScore=0;
     }
     
     public String getID (){
@@ -36,8 +36,7 @@ public class Student {
     public int getQuizCount (){
     	return quizCount;
     }
-    //score per question is not stated
-    public double getTotalScore (){
+    public int getTotalScore (){
     	return totalScore;
     }
     
@@ -47,12 +46,6 @@ public class Student {
     public void setName (String studName) {
     	name = studName;
     }
-    public void setQuizCount (int new_quizCount) {
-    	quizCount = new_quizCount;
-    }
-    //public void setTotalScore (double new_quizScore) {
-    	//totalScore = new_quizScore;
-    //}
     
     public void addQuiz (int score) {
     	quizCount++;
@@ -60,6 +53,6 @@ public class Student {
     }
     
     public double getAverageScore() {
-    	return totalScore/quizCount;
+    	return totalScore/(double)quizCount;
     }
 }
