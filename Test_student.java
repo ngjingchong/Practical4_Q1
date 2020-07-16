d85c6ed60ad527c2a3cf53ccc86537e34fcd6ccd
ngjingchong <66916397+ngjingchong@users.noreply.github.com>
2020-07-16 10:54:26 +0800

    Update Test_student.java

--- Test_student.java
+++ Test_student.java
@@ -6,22 +6,17 @@
  * @version 1.00 2020/7/12
  */
 
-
 public class Test_Student {
 
     public static void main (String [] args) {
     	Student stud1 = new Student("1902099WMD", "jchong");
     	Student stud2 = new Student();
     	
-    	stud1.setQuizCount(10);
-    	stud1.setTotalScore(18);
-    	
     	stud2.setID("1234abc");
     	stud2.setName("test student");
-    	stud2.setQuizCount(10);
-    	stud2.setTotalScore(15);
+    	stud2.addQuiz(10);
+    	stud2.addQuiz(8);
     	
-    	stud1.addQuiz(2);
     	System.out.println("Student ID : " + stud1.getID());
     	System.out.println("Student Name : " + stud1.getName());
     	System.out.println("Total Quiz Takken : " + stud1.getQuizCount());
